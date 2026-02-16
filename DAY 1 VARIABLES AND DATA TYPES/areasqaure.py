48594b4340296a55b2780cd3c5e7fc05cf1078dd
shubhambhamare07 <shubhamhehe23@gmail.com>
2026-02-16 22:10:51 +0530

    Day 1 - Variables and Data types

---
+++ DAY 1 VARIABLES AND DATA TYPES/areasqaure.py
@@ -0,0 +1,3 @@
+a=float(input("enter side of sqaure"))
+area = a*a
+print("area of sqaure is",area)
---
+++ DAY 1 VARIABLES AND DATA TYPES/average.py
@@ -0,0 +1,4 @@
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+avg=(a+b)/2
+print("average is ",avg)
