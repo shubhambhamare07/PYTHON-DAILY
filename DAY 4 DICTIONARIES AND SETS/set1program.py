eac2c8d8165da1ec62413b53f3f588c18f043a4e
shubhambhamare07 <shubhamhehe23@gmail.com>
2026-02-19 21:37:39 +0530

    DAY 4

---
+++ DAY 4 DICTIONARIES AND SETS/set1program.py
@@ -0,0 +1,2 @@
+set1= {"python","java","c++","python","javascript","java","python","java","c++","C"}
+print("CLASS REQUIRED FOR EACH SUBJECT BY STUDENT IS", len(set1))
---
+++ DAY 4 DICTIONARIES AND SETS/set2program.py
@@ -0,0 +1,3 @@
+#to save same number as int and float in set
+set1 = {9,"9.0"}
+print(set1)
